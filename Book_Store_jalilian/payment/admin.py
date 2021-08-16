afa80d077acb8d9554b6d60d6f736a57d77735fb
zahrajalilian <81348506+zahrajalilian@users.noreply.github.com>
2021-08-16 20:41:02 +0430

    Update admin.py

--- Book_Store_jalilian/payment/admin.py
+++ Book_Store_jalilian/payment/admin.py
@@ -5,9 +5,11 @@ here we register our invoice and invoiceline
 from django.contrib import admin
 
 # Register your models here.
-from .models import Invoice,InvoiceLine
+from .models import Invoice,InvoiceLine,BasketDiscount,ProductDiscount
 
 
 admin.site.register(Invoice)
 admin.site.register(InvoiceLine)
+admin.site.register(BasketDiscount)
+admin.site.register(ProductDiscount)
 
