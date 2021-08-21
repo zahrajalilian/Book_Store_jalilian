440810b4aa0bc30c35a901515365524cbf32eca3
zahrajalilian <81348506+zahrajalilian@users.noreply.github.com>
2021-08-21 13:22:08 +0430

    Update models.py

--- Book_Store_jalilian/accounts/models.py
+++ Book_Store_jalilian/accounts/models.py
@@ -1,14 +1,72 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django_countries.fields import CountryField
-
 # Create your models here.
+from django_countries.fields import CountryField
+# Create your models here.
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        """
+        Creates and saves a User with the given email, username
+         and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staff(self, email, username, password=None):
+        """
+        Creates and saves a staff with the given email, username
+        and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            username=username,
+        )
+        user.is_admin = False
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None):
+
+            """
+            Creates and saves a superuser with the given email, username
+             and password.
+            """
+            user = self.create_user(
+                email,
+                password=password,
+                username=username,
+            )
+            user.is_admin = True
+            user.is_staff = True
+            user.save(using=self._db)
+            return user
+
+
+
+
+
+
+
 
 
 class Address(models.Model):
     """
-    a model with many to many relationship with CustomUser
-    """
+       a model with many to many relationship with CustomUser
+       """
 
     street = models.CharField(max_length=200)
     street_2 = models.CharField(max_length=200, blank=True, null=True)
@@ -19,6 +77,7 @@ class Address(models.Model):
 
     def __str__(self):
         return f' {self.country},{self.state},{self.city}, {self.street} '
+#
 
 
 """
@@ -32,16 +91,35 @@ admin
 
 
 class CustomUser(AbstractUser):
-    is_customer = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_admin = models.BooleanField(default=False)
+    GENDER_CHOICES = [('M', 'Male'), ('F', 'Female'), ('U', 'Unknown')]
+    username = models.CharField(max_length=200,unique=True)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    company = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    gender = models.CharField(verbose_name='gender', choices=GENDER_CHOICES, default='U', max_length=1)
+    company = models.CharField(max_length=200,null=True,blank=True)
     address = models.ManyToManyField(Address)
     phone = models.IntegerField(null=True, blank=True)
     fax = models.IntegerField(null=True, blank=True)
+    # objects = MyUserManager()
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+
+class CustomerProxy(CustomUser):
+    class Meta:
+        proxy = True
+
+
+class StaffProxy(CustomUser):
+    class Meta:
+        proxy = True
+        # permissions=['']
+
+class AdminProxy(CustomUser):
+    class Meta:
+        proxy = True
+
+
 
