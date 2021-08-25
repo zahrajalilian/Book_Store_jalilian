2f8ec44dee11fd69dc61c356ff176e2646128553
zahrajalilian <81348506+zahrajalilian@users.noreply.github.com>
2021-08-25 20:57:17 +0430

    Update admin.py

--- Book_Store_jalilian/accounts/admin.py
+++ Book_Store_jalilian/accounts/admin.py
@@ -1,20 +1,8 @@
-"""
-import here
-""
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-<<<<<<< HEAD
-from .models import CustomUser, Address,CustomerProxy,StaffProxy,AdminProxy
-
-"""
-here we register our models
-"""
-=======
-# from .models import CustomUser,Address,
 from .models import *
->>>>>>> 440810b4aa0bc30c35a901515365524cbf32eca3
-
 
 admin.site.register(Address)
 
@@ -28,6 +16,9 @@ class CustomUserAdmin(UserAdmin):
 
     (None, {'fields': ('address',)}),
     )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('phone',)}),
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
@@ -39,6 +30,7 @@ class CustomerAdmin(admin.ModelAdmin):
         return CustomUser.objects.filter(is_staff=False,is_superuser=False)
     list_display = ['email','username']
 
+
 @admin.register(StaffProxy)
 class StaffAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
@@ -46,14 +38,10 @@ class StaffAdmin(admin.ModelAdmin):
 
     list_display = ['email', 'username']
 
+
 @admin.register(AdminProxy)
 class AdminAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         return CustomUser.objects.filter(is_staff=True,is_superuser=True)
 
-<<<<<<< HEAD
     list_display = ['email', 'username']
-=======
-    list_display = ['email', 'username']"
-
->>>>>>> 440810b4aa0bc30c35a901515365524cbf32eca3
